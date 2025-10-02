b1b19365a7e33f3b9b1ac1d47f226e6af478229b
maks1231-crypto <120469340+maks1231-crypto@users.noreply.github.com>
2025-10-03 00:49:00 +0300

    Update gtdsxkfk.lua

--- gtdsxkfk.lua
+++ gtdsxkfk.lua
@@ -19,14 +19,14 @@ remotes.ChangeTickSpeed:InvokeServer(3)
 local difficulty = "dif_impossible"
 local placements = {
     {
-        time = 29, unit = "unit_lawnmower", slot = "1",
+        time = 31, unit = "unit_lawnmower", slot = "1",
         data = {Valid=true,PathIndex=3,Position=Vector3.new(-843.87384,62.1803055,-123.052032),
             DistanceAlongPath=248.0065,
             CF=CFrame.new(-843.87384,62.1803055,-123.052032,-0,0,1,0,1,-0,-1,0,-0),
             Rotation=180}
     },
     {
-        time = 33, unit = "unit_rafflesia", slot = "2",
+        time = 35, unit = "unit_rafflesia", slot = "2",
         data = {
             Valid = true,
             PathIndex = 3,
@@ -37,7 +37,7 @@ local placements = {
         }
     },
     {
-        time = 41, unit = "unit_rafflesia", slot = "2",
+        time = 43, unit = "unit_rafflesia", slot = "2",
         data = {
             Valid = true,
             PathIndex = 1,
@@ -48,7 +48,7 @@ local placements = {
         }
     },
     {
-        time = 56, unit = "unit_rafflesia", slot = "2",
+        time = 58, unit = "unit_rafflesia", slot = "2",
         data = {
             Valid = true,
             PathIndex = 3,
@@ -59,7 +59,7 @@ local placements = {
         }
     },
     {
-        time = 81, unit = "unit_rafflesia", slot = "2",
+        time = 83, unit = "unit_rafflesia", slot = "2",
         data = {
             Valid = true,
             PathIndex = 2,
@@ -93,3 +93,4 @@ end
 
 loadstring(game:HttpGet("https://pastebin.com/raw/HkAmPckQ"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hassanxzayn-lua/Anti-afk/main/antiafkbyhassanxzyn"))()
+
